55d36f8119cbe93ef3ad3bc244b9c48318091d42
Lucas Lima <lucascglima@gmail.com>
2023-03-04 21:39:48 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
-/** @type {import('next').NextConfig} */
 const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "css")],
+  },
+  trailingSlash: true,
+  devIndicators: {
+    buildActivity: false,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  env: {
-    user: process.env.NEXT_PUBLIC_USER,
-    code: process.env.NEXT_PUBLIC_CODE,
+  experimental: {
+    forceSwcTransforms: false,
   },
 };
