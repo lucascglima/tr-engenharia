79a662b92b6584ab5c20ee03d4b75128f52fe587
Lucas Lima <lucascglima@gmail.com>
2023-03-05 12:09:09 -0300

    email contact template

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,3 +1,4 @@
+import "../../../public/img/logo.png";
 export default async function (req, res) {
   let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
@@ -14,15 +15,101 @@ export default async function (req, res) {
     to: process.env.NEXT_PUBLIC_USER,
     subject: "Contato recebido - " + req.body.name,
     html: `
-      <div>
-      <p>
-      Nome: <strong>${req.body.name}</strong> <br/>
-      E-mail: <strong>${req.body.email}</strong> <br/>
-      Telefone (Whatsapp): <strong>${req.body.phoneNumber}</strong> <br/>
-      Assunto: <strong>${req.body.subject}</strong> <br/><br/>      Mensagem:<br/>
-        ${req.body.message}
-      </p>
-      </div>`,
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <title>Contato recebido pelo site</title>
+    <style>
+        /* Email body styles */
+        body {
+            font-family: 'Lato', sans-serif;
+            font-size: 16px;
+            line-height: 1.6;
+            color: #333333;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Email container styles */
+        .email-container {
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 30px;
+            background-color: #ffffff;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        /* Email header styles */
+        .email-header {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        /* Email heading styles */
+        .email-heading {
+            font-size: 24px;
+            font-weight: bold;
+            margin: 0;
+            color: #2f3d4a;
+            margin-top: 12px;
+        }
+
+        /* Email message styles */
+        .email-message {
+            margin-bottom: 30px;
+        }
+
+        /* Email signature styles */
+        .email-signature {
+            margin-top: 30px;
+            text-align: right;
+        }
+
+        /* Email signature text styles */
+        .email-signature-text {
+            font-size: 18px;
+            font-style: italic;
+            color: #999999;
+            margin: 0;
+        }
+
+        /* Email signature name styles */
+        .email-signature-name {
+            font-size: 20px;
+            font-weight: bold;
+            color: #2f3d4a;
+            margin: 5px 0 0 0;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="email-container">
+        <div class="email-header">
+            <img src="https://www.trenergiasolar.eco.br/img/tr/logo-min.svg" alt="Solar Energy Company Logo">
+            <h1 class="email-heading">Recebemos um contato pelo site</h1>
+        </div>
+        <div class="email-message">
+              Nome: <strong>${req.body.name}</strong> <br/>
+              E-mail: <strong>${req.body.email}</strong> <br/>
+              Telefone (Whatsapp): <strong>${req.body.phoneNumber}</strong> <br/>
+              Assunto: <strong>${req.body.subject}</strong> <br/><br/>  
+              Mensagem:<br/>
+              ${req.body.message}
+        </p>  
+            <div class="email-signature">
+                <p class="email-signature-text">TR Energia solar</p>
+            </div>
+        </div>
+    </div>
+  </body>
+  </html>`,
   };
   let info = await transporter.sendMail(mailData);
   console.log(info, "TO TR");
@@ -30,19 +117,99 @@ export default async function (req, res) {
   const mailDataClient = {
     from: process.env.NEXT_PUBLIC_USER,
     to: req.body.email,
-    subject: "TR Energia Solar - Recebemos seu contato.",
+    subject: "TR Energia Solar - Contato recebido",
     html: `
-      <div>
-      <p>
-      Olá  <strong>${req.body.name}</strong>, <br/>
-      <p>
-        Ficamos felizes em receber seu contato, nosso time entrará em contato com você em breve. <nr/>
-        Assunto: <strong>${req.body.subject}</strong> <br/><br/>
-        Mensagem:<br/>
-        ${req.body.message}
-      </p>       
-      </p>
-      </div>`,
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <title>Obrigado por nos contatar</title>
+    <style>
+        /* Email body styles */
+        body {
+            font-family: 'Lato', sans-serif;
+            font-size: 16px;
+            line-height: 1.6;
+            color: #333333;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Email container styles */
+        .email-container {
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 30px;
+            background-color: #ffffff;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        /* Email header styles */
+        .email-header {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        /* Email heading styles */
+        .email-heading {
+            font-size: 24px;
+            font-weight: bold;
+            margin: 0;
+            color: #2f3d4a;
+        }
+
+        /* Email message styles */
+        .email-message {
+            margin-bottom: 30px;
+        }
+
+        /* Email signature styles */
+        .email-signature {
+            margin-top: 30px;
+            text-align: right;
+        }
+
+        /* Email signature text styles */
+        .email-signature-text {
+            font-size: 18px;
+            font-style: italic;
+            color: #999999;
+            margin: 0;
+        }
+
+        /* Email signature name styles */
+        .email-signature-name {
+            font-size: 20px;
+            font-weight: bold;
+            color: #2f3d4a;
+            margin: 5px 0 0 0;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="email-container">
+        <div class="email-header">
+            <img src="https://www.trenergiasolar.eco.br/img/tr/logo-min.svg" alt="Solar Energy Company Logo">
+            <h1 class="email-heading">Obrigado por nos contatar!</h1>
+        </div>
+        <div class="email-message">
+            <p>Prezado ${req.body.name},</p>
+            <p>Recebemos sua mensagem e agradecemos seu interesse em nossa empresa de energia solar. Nossa equipe irá analisar sua mensagem e retornaremos o mais breve possível.</p>
+            <p>Obrigado por nos considerar como seu parceiro em energia renovável.</p>
+            <p>Atenciosamente,</p>
+            <div class="email-signature">
+                <p class="email-signature-text">TR Energia solar</p>
+            </div>
+        </div>
+    </div>
+  </body>
+  </html>`,
   };
 
   let infoClient = await transporter.sendMail(mailDataClient);
