7c94cfb439b6bc6bb710669b137328f323a19b5c
Lucas Lima <lucascglima@gmail.com>
2023-03-04 21:21:21 -0300

    remove next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "css")],
-  },
-  trailingSlash: true,
-  devIndicators: {
-    buildActivity: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  env: {
-    user: process.env.NEXT_PUBLIC_USER,
-    code: process.env.NEXT_PUBLIC_CODE,
-  },
-  pageExtensions: ["contact.js"],
-};
+// module.exports = {
+//   // reactStrictMode: true,
+//   // sassOptions: {
+//   //   includePaths: [path.join(__dirname, "css")],
+//   // },
+//   // trailingSlash: true,
+//   // devIndicators: {
+//   //   buildActivity: false,
+//   // },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   experimental: {
+//     forceSwcTransforms: false,
+//   },
+//   // env: {
+//   //   user: process.env.NEXT_PUBLIC_USER,
+//   //   code: process.env.NEXT_PUBLIC_CODE,
+//   // },
+// };
