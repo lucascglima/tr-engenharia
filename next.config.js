8840e4bb871a04f4e94a818104b02b519c93aaa8
Lucas Lima <lucascglima@gmail.com>
2023-03-04 22:08:22 -0300

    update fetch

--- next.config.js
+++ next.config.js
@@ -12,9 +12,6 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    forceSwcTransforms: false,
-  },
   env: {
     user: process.env.NEXT_PUBLIC_USER,
     code: process.env.NEXT_PUBLIC_CODE,
