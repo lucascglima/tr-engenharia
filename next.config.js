aaab3b74962abba59a8e1e0fd2e549668a444c8b
Lucas Lima <lucascglima@gmail.com>
2023-03-04 21:36:16 -0300

    update next config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+module.exports = {
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "css")],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    user: process.env.NEXT_PUBLIC_USER,
+    code: process.env.NEXT_PUBLIC_CODE,
+  },
+};
