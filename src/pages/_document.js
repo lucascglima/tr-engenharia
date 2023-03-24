6f8efb5ef28d49d55f3096a1603e61fa084c9d66
Lucas Lima <lucascglima@gmail.com>
2023-03-24 18:43:06 -0300

    head for seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,17 +10,25 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt">
         <Head>
+          <title>
+            TR Energia Solar - Energia solar, redução na conta de luz.
+          </title>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="keywords" content="TR Energia Solar" />
-          <meta name="description" content="TR Energia Solar" />
+          <meta
+            name="keywords"
+            content="TR,  energia Solar, recife, pernambuco, redução na conta"
+          />
+          <meta
+            name="description"
+            content="Oferecemos soluções em energia solar para redução na conta de luz. Entre em contato conosco para saber mais."
+          />
           <meta name="author" content="Lucas Lima" />
           <link
             href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
             rel="stylesheet"
           />
-
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-WE3ST3KW82"
